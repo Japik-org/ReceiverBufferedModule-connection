0a2a908c3be7990d52919f563fb38f2b0a8d4511
pro100kryto <pro100kryto.original@gmail.com>
2020-12-27 18:44:33 -0300

    Create project and implement

---
+++ src/main/java/com/pro100kryto/server/modules/senderreceiverbuffered/connection/IReceiverBufferedModuleConnection.java
@@ -0,0 +1,11 @@
+package com.pro100kryto.server.modules.senderreceiverbuffered.connection;
+
+import com.pro100kryto.server.module.IModuleConnection;
+import com.pro100kryto.server.utils.datagram.packets.IPacket;
+import com.sun.istack.Nullable;
+
+public interface IReceiverBufferedModuleConnection extends IModuleConnection {
+    boolean hasNextPacket();
+    @Nullable
+    IPacket getNextPacket();
+}
