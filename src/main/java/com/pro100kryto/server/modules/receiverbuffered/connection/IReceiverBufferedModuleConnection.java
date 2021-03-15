165db4f0b58dc269329814343481ba1510310c24
pro100kryto <pro100kryto.original@gmail.com>
2021-03-15 02:30:12 -0300

    Replace istack with jetbrains annotations

--- src/main/java/com/pro100kryto/server/modules/receiverbuffered/connection/IReceiverBufferedModuleConnection.java
+++ src/main/java/com/pro100kryto/server/modules/receiverbuffered/connection/IReceiverBufferedModuleConnection.java
@@ -2,7 +2,7 @@ package com.pro100kryto.server.modules.receiverbuffered.connection;
 
 import com.pro100kryto.server.module.IModuleConnection;
 import com.pro100kryto.server.utils.datagram.packets.IPacket;
-import com.sun.istack.Nullable;
+import org.jetbrains.annotations.Nullable;
 
 public interface IReceiverBufferedModuleConnection extends IModuleConnection {
     boolean hasNextPacket();
